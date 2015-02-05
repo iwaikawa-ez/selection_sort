af75a25005ae65bc3b8751815bd440afb99b9180
iwaikawa-ez <iwaikawa@exvisionz.jp>
2015-02-05 19:23:10 +0900

    実行結果OK。swap関数付近は要修正。

--- sort.php
+++ sort.php
@@ -1,5 +1,4 @@
 <?php
-/*
  function selection_sort($data,$is_order_by_ascending){
   	$max = count($data) - 1;
 
@@ -7,7 +6,9 @@
       for($a = 0; $a < $max; $a++){
         for($i = $a + 1; $i <= $max; $i++){
   			  if($data[$a] > $data[$i]){
-            swap($a,Si);
+            $x = $data[$a];
+            $y = $data[$i];
+            list($data[$a],$data[$i]) = swap($x,$y);
           }
 	  	  }
   	  }
@@ -15,27 +16,20 @@
 	    for($a = 0; $a < $max; $a++){
 	  	  for($i = $a + 1; $i <= $max; $i++){
 	  		  if($data[$a] < $data[$i]){
-	  			  swap($a,$i);
+	          $x = $data[$a];
+            $y = $data[$i];
+            list($data[$a],$data[$i]) = swap($x,$y);
 	  	  	}
         }
       }
     }
     return $data;
   }
-*/
-
-  $x = 10;
-  $y = 20;
 
   function swap($x,$y){
     return array($y,$x);
   }
 
-  list($x,$y) = swap($x,$y);
-  echo "{$x},{$y}\n";
-
-/*
   print_r(selection_sort(array(7,5,10,1,3,2,6,8),true));
   print_r(selection_sort(array(7,5,10,1,3,2,6,8),false));
-*/
 ?>
