8af2448b728acea16deb27e57aa24d6a42f5d0b5
iwaikawa-ez <iwaikawa@exvisionz.jp>
2015-02-05 19:11:15 +0900

    関数のネストの順番を修正。関数テストの変数を修正。

--- sort.php
+++ sort.php
@@ -1,31 +1,6 @@
 <?php
-  $a = 10;
-  $b = 20;
-
-  function swap($a,$b){
-    return array($b,$a);
-  }
-
-  list($a,$b) = swap($a, $b);
-  echo "{$a},{$b}\n";
-
 /*
-  function swap($data[$a],$data[$i]){
-      $data2[$a] = $data[$i];
-      $data2[$i] = $data[$a];
-
-      return $data2();
-    }
-
-
-
-  function swap($a,$i){
-	  $x = $data[$a];
-		$data[$a] = $data[$i];
-		$data[$i] = $x;
-  }
-
-  function selection_sort($data,$is_order_by_ascending){
+ function selection_sort($data,$is_order_by_ascending){
   	$max = count($data) - 1;
 
     if($is_order_by_ascending == true){
@@ -47,9 +22,20 @@
     }
     return $data;
   }
+*/
 
+  $x = 10;
+  $y = 20;
+
+  function swap($x,$y){
+    return array($y,$x);
+  }
+
+  list($x,$y) = swap($x,$y);
+  echo "{$x},{$y}\n";
+
+/*
   print_r(selection_sort(array(7,5,10,1,3,2,6,8),true));
   print_r(selection_sort(array(7,5,10,1,3,2,6,8),false));
 */
-
 ?>
