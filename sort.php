974c6386699abb31308ff97120ba4a0666403125
iwaikawa-ez <iwaikawa@exvisionz.jp>
2015-02-06 10:58:30 +0900

    swapを関数ではなく"list($a,$b)=array($b,$a)"の様に表現

--- sort.php
+++ sort.php
@@ -1,14 +1,11 @@
 <?php
  function selection_sort($data,$is_order_by_ascending){
   	$max = count($data) - 1;
-
     if($is_order_by_ascending == true){
       for($a = 0; $a < $max; $a++){
         for($i = $a + 1; $i <= $max; $i++){
   			  if($data[$a] > $data[$i]){
-            $x = $data[$a];
-            $y = $data[$i];
-            list($data[$a],$data[$i]) = swap($x,$y);
+            list($data[$a],$data[$i]) = array($data[$i],$data[$a]);
           }
 	  	  }
   	  }
@@ -16,20 +13,13 @@
 	    for($a = 0; $a < $max; $a++){
 	  	  for($i = $a + 1; $i <= $max; $i++){
 	  		  if($data[$a] < $data[$i]){
-	          $x = $data[$a];
-            $y = $data[$i];
-            list($data[$a],$data[$i]) = swap($x,$y);
+            list($data[$a],$data[$i]) = array($data[$i],$data[$a]);
 	  	  	}
         }
       }
     }
     return $data;
   }
-
-  function swap($x,$y){
-    return array($y,$x);
-  }
-
   print_r(selection_sort(array(7,5,10,1,3,2,6,8),true));
   print_r(selection_sort(array(7,5,10,1,3,2,6,8),false));
 ?>
