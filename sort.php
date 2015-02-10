7f66b203a3033a304b3d010133e6d74fe6dc1bf2
iwaikawa-ez <iwaikawa@exvisionz.jp>
2015-02-10 16:40:08 +0900

    参照渡しを用いたswap関数の導入`

--- sort.php
+++ sort.php
@@ -1,23 +1,27 @@
 <?php
- function selection_sort($data,$is_order_by_ascending){
+ function selection_sort($data, $is_order_by_ascending) {
   	$max = count($data) - 1;
-    if($is_order_by_ascending == true){
-      for($a = 0; $a < $max; $a++){
-        for($i = $a + 1; $i <= $max; $i++){
-  			  if($data[$a] > $data[$i]){
-            list($data[$a],$data[$i]) = array($data[$i],$data[$a]);
+    if ($is_order_by_ascending == true) {
+      for ($a = 0; $a < $max; $a++) {
+        for ($i = $a + 1; $i <= $max; $i++) {
+  			  if ($data[$a] > $data[$i]) {
+            swap($data, $a, $i);
           }
 	  	  }
   	  }
-    }else{
-	    for($a = 0; $a < $max; $a++){
-	  	  for($i = $a + 1; $i <= $max; $i++){
-	  		  if($data[$a] < $data[$i]){
-            list($data[$a],$data[$i]) = array($data[$i],$data[$a]);
+    } else {
+	    for ($a = 0; $a < $max; $a++) {
+	  	  for ($i = $a + 1; $i <= $max; $i++) {
+	  		  if ($data[$a] < $data[$i]) {
+            swap($data, $a, $i);
 	  	  	}
         }
       }
     }
     return $data;
   }
-?>
+
+  function swap(&$data, $a, $i)
+  {
+    return list($data[$a], $data[$i]) = array($data[$i], $data[$a]);
+  }
--- sort_test.php
+++ sort_test.php
@@ -1,28 +1,27 @@
 <?php
-  require_once('sort.php');
+require_once('sort.php');
 
-  class sortTest extends PHPUnit_Framework_TestCase
+class sortTest extends PHPUnit_Framework_TestCase
+{
+  public function test_can_asc_sort()
   {
-    // 昇順ソートができる
-    public function test_can_asc_sort()
-    {
-      $list = array(7, 5, 10, 1, 3, 2, 6, 8);
-
-      $result = selection_sort($list, true);
-      $assert = array(1, 2, 3, 5, 6, 7, 8, 10);
-
-      $this->assertEquals($result, $assert);
-    }
-
-    // 降順ソートができる
-    public function test_can_desc_sort()
-    {
-      $list = array(7, 5, 10, 1, 3, 2, 6, 8);
-
-      $result = selection_sort($list, false);
-      $assert = array(10, 8, 7, 6, 5, 3, 2, 1);
-
-      $this->assertEquals($result, $assert);
-    }
+    $list = array(7, 5, 10, 1, 3, 2, 6, 8);
+    $result = selection_sort($list, true);
+    $assert = array(1, 2, 3, 5, 6, 7, 8, 10);
+    $this->assertEquals($result, $assert);
   }
-?>
+
+  public function test_can_desc_sort()
+  {
+    $list = array(7, 5, 10, 1, 3, 2, 6, 8);
+    $result = selection_sort($list, false);
+    $assert = array(10, 8, 7, 6, 5, 3, 2, 1);
+    $this->assertEquals($result, $assert);
+  }
+
+  public function test_swap()
+  {
+    $array = array(10, 20);
+    $this->assertEquals(swap($array, 0, 1), array(20, 10));
+  }
+}
