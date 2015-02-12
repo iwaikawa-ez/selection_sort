ed733058542f72226f8490998b2639315f179558
iwaikawa-ez <iwaikawa@exvisionz.jp>
2015-02-12 12:37:25 +0900

    if文の改行

--- sort.php
+++ sort.php
@@ -3,35 +3,13 @@ function selection_sort($data, $is_order_by_ascending) {
   $max = count($data) - 1;
   for ($a = 0; $a < $max; $a++) {
     for ($i = $a + 1; $i <= $max; $i++) {
-      if (($is_order_by_ascending == true && $data[$a] > $data[$i]) || ($is_order_by_ascending == false && $data[$a] < $data[$i])) swap($data[$a], $data[$i]);
+      if (($is_order_by_ascending == true && $data[$a] > $data[$i]) ||
+        ($is_order_by_ascending == false && $data[$a] < $data[$i])) 
+          swap($data[$a], $data[$i]);
     }
   }
  return $data;
 }
-
-/*
-function selection_sort($data, $is_order_by_ascending) {
-  $max = count($data) - 1;
-  if ($is_order_by_ascending == true) {
-    for ($a = 0; $a < $max; $a++) {
-      for ($i = $a + 1; $i <= $max; $i++) {
-        if ($data[$a] > $data[$i]) {
-          swap($data[$a], $data[$i]);
-        }
-      }
-    }
-  } else {
-    for ($a = 0; $a < $max; $a++) {
-      for ($i = $a + 1; $i <= $max; $i++) {
-        if ($data[$a] < $data[$i]) {
-          swap($data[$a], $data[$i]);
-        }
-      }
-    }
-  }
-  return $data;
-}
-*/
 
 function swap(&$x, &$y)
 {
